e3619787bb00679959975b67d7fd81007f37d6b0
tianqin <387956977@qq.com>
2019-09-29 22:40:31 +0800

    init

---
+++ spring-boot-data-jdbc-01/src/main/java/com/mengxuegu/springbootdatajdbc01/SpringBootDataJdbc01Application.java
@@ -0,0 +1,13 @@
+package com.mengxuegu.springbootdatajdbc01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataJdbc01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDataJdbc01Application.class, args);
+    }
+
+}
